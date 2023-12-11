575041b9601736ae9fcee3f18b882196046d2ca5
mukund kumar <mukund5457@gmail.com>
2023-12-11 17:34:18 +0530

    added clipboard feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import regeneratorRuntime from 'regenerator-runtime'
 import SpeechRecognition,{useSpeechRecognition} from 'react-speech-recognition'
+import useClipboard from 'react-use-clipboard'
 function App() {
- 
+ const [textToCopy,setTextToCopy]=useState();
+ const [isCopied,setCopied]=useClipboard(textToCopy,{
+  successDuration:1000
+ });
+
   const {
     transcript,
     resetTranscript,
@@ -20,20 +25,22 @@ function App() {
         <h2>Speech To Text Converter</h2>
         <p>A React hook that convert speech from the microphone to text and make it available in react components.</p>
 
-        <div className='main-content'>
+        <div className='main-content' onClick={()=>setTextToCopy(transcript)}>
         
         {transcript}
 
         </div>
         <div className='btn-style'>
-          <button>Copy</button>
+          <button onClick={setCopied}>
+            {isCopied ? 'Copied' : 'Copy'}
+            </button>
           <button onClick={SpeechRecognition.startListening({continuous:true, language:'en-IN'})}>Start Listening</button>
           <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
           <button onClick={resetTranscript}>Reset</button>
         </div>
-        <h1>hii </h1>
+       
       </div>
-      <h1>hii</h1>
+      
     </>
   )
 }
